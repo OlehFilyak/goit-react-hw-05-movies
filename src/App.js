f5cfb72b95902f2c89e0c6de94a6c36cc8a1461f
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-07-31 15:57:29 +0300

    added main routing, lazy-loadind, suspend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
+import React, { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Header from "./Components/Header";
+// import MoviesView from "./views/MoviesView";
+// import HomeView from "./views/HomeView";
+// import NotFoundPage from "./views/NotFoundPage";
+// import MovieCardView from "./views/MovieCardView";
+import Spinner from "./Components/Spinner";
+
 import "./App.css";
 
+const HomeView = lazy(() =>
+  import("./views/HomeView" /* webpackChunkName: "home-page" */)
+);
+const MoviesView = lazy(() =>
+  import("./views/MoviesView" /* webpackChunkName: "MoviesView" */)
+);
+const NotFoundPage = lazy(() =>
+  import("./views/NotFoundPage" /* webpackChunkName: "NotFoundPage" */)
+);
+const MovieCardView = lazy(() =>
+  import("./views/MovieCardView" /* webpackChunkName: "MovieCardView" */)
+);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomeView />
+          </Route>
+
+          <Route path="/movies" exact>
+            <MoviesView />
+          </Route>
+
+          <Route path="/movies/:movieId">
+            <MovieCardView />
+          </Route>
+
+          <Route>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
